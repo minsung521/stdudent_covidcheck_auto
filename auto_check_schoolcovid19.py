46fe2833a0109fac61d1c19d72ef84764173dc5a
mingsung-dev <mingsung521@gmail.com>
2021-01-08 09:41:49 +0900

    일단 들어가긴 했는데.. select 태그를 어떻게 할지 모르겠다..

--- auto_check_schoolcovid19.py
+++ auto_check_schoolcovid19.py
@@ -1,33 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
 
 import time
 
-driver = webdriver.Chrome('P:\\stdudent_covidcheck_auto\chromedriver.exe')
-url = 'https://eduro.ice.go.kr/stv_cvd_co00_002.do'
+driver = webdriver.Chrome('D:\stdudent_covidcheck_auto-\chromedriver.exe')
+url = 'https://hcs.eduro.go.kr/#/loginHome'
 driver.get(url)
 driver.maximize_window()
 action = ActionChains(driver)
-
-(
-    action.send_keys('용현중학교').key_down(Keys.TAB).pause(1)
-    .send_keys('김민성').key_down(Keys.TAB).pause(1)
-    .send_keys('050201').pause(1)
-    .perform()
-)
-driver.find_element_by_css_selector('#btnConfirm').click()
-time.sleep(1)
-driver.find_element_by_css_selector(f'#rspns011').click()
-time.sleep(1)
-driver.find_element_by_css_selector('#rspns02').click()
-time.sleep(1)
-driver.find_element_by_css_selector('#rspns070').click()
-time.sleep(1)
-driver.find_element_by_css_selector('#rspns080').click()
-time.sleep(1)
-driver.find_element_by_css_selector('#rspns090').click()
-time.sleep(1)
-driver.find_element_by_css_selector('#rspns090').click()
-driver.find_element_by_css_selector('#btnConfirm').click()
-time.sleep(1)
+#
+# (
+#     action.send_keys('용현중학교').key_down(Keys.TAB).pause(1)
+#     .send_keys('김민성').key_down(Keys.TAB).pause(1)
+#     .send_keys('050201').pause(1)
+#     .perform()
+# )
+driver.find_element_by_css_selector('#btnConfirm2').click()
+time.sleep(0.5)
+driver.find_element_by_css_selector('#schul_name_input').click()
+time.sleep(0.5)
+driver.find_element_by_css_selector('#sidolabel').click()
+time.sleep(0.5)
+# driver.find_element_by_css_selector('#rspns02').click()
+# time.sleep(1)
+# driver.find_element_by_css_selector('#rspns070').click()
+# time.sleep(1)
+# driver.find_element_by_css_selector('#rspns080').click()
+# time.sleep(1)
+# driver.find_element_by_css_selector('#rspns090').click()
+# time.sleep(1)
+# driver.find_element_by_css_selector('#rspns090').click()
+# driver.find_element_by_css_selector('#btnConfirm').click()
+# time.sleep(1)
